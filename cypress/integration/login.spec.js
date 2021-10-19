e2d721206af1dfc26c9b6ffdf6ac1a5c7d738163
Patrícia Machado <patriciamachado1993@gmail.com>
2021-10-19 19:21:35 -0300

    cenários pré cadastro e produtos

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,8 +2,15 @@
 
 context('Funcionalidade Login', () => {
 
-    it('Deve fazer Login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta')
+    })
+
+    //afterEach(() => {
+        //cy.screenshot()
+    //});
+
+    it('Deve fazer Login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -12,7 +19,6 @@ context('Funcionalidade Login', () => {
     })
 
     it('Deve exibir mensagem de erro ao inserir usuário inválido', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta')
         cy.get('#username').type('ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -21,7 +27,6 @@ context('Funcionalidade Login', () => {
     })
 
     it('Deve exibir mensagem de erro ao inserir senha inválida', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
